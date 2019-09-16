4b815136526cb616e2ed20a5e67518c5e7ee7034
Chuck <chuck@intelligence.org>
2019-09-16 10:35:51 -0700

    Add maintainer

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1171,6 +1171,12 @@
     githubId = 30435868;
     name = "Okina Matara";
   };
+  chkno = {
+    email = "chuck@intelligence.org";
+    github = "chkno";
+    githubId = 1118859;
+    name = "Scott Worley";
+  };
   choochootrain = {
     email = "hurshal@imap.cc";
     github = "choochootrain";
